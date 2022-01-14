a073907cc945b681f09c4deeb0c0467d43909603
morteza-shahrabi-farahani <morteza.shahrabii@gmail.com>
2022-01-14 13:10:16 +0330

    add first question

---
+++ سوال زرد/Readme.txt
@@ -0,0 +1,2 @@
+question link: 
+https://quera.ir/problemset/3537/
---
+++ سوال زرد/answer.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main () {
+	int n;
+	scanf("%d" , &n);
+	if(n==1) {
+		printf("Wow!");
+	}
+		if(n==2) {
+		printf("Woow!");
+	}
+		if(n==3) {
+		printf("Wooow!");
+	}
+		if(n==4) {
+		printf("Woooow!");
+	}
+		if(n==5) {
+		printf("Wooooow!");
+	}
+		if(n==6) {
+		printf("Woooooow!");
+	}
+		if(n==7) {
+		printf("Wooooooow!");
+	}
+		if(n==8) {
+		printf("Woooooooow!");
+	}
+		if(n==9) {
+		printf("Wooooooooow!");
+	}
+		if(n==10) {
+		printf("Woooooooooow!");
+	}
+}
+
